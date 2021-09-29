c523db9e12445b2b83ba00549ea04db1a47b7ceb
Kyle Schrader <kyle.schrader97@gmail.com>
2021-09-29 16:16:32 -0500

    created Student class in main. added getters and setters plus private attributes

---
+++ src/main/java/Student.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(ArrayList<Integer> grades) {
+        this.grades = grades;
+    }
+
+    public double getGradeAverage() {
+        return 0.0;
+    }
+}
